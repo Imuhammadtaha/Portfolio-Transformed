0d1db18a25a92d466967262487cf5618d6496dbb
Imuhammadtaha <145789772+Imuhammadtaha@users.noreply.github.com>
2024-08-11 15:50:49 +0500

    Update ContactSection.js

--- frontend/src/components/ContactSection.js
+++ frontend/src/components/ContactSection.js
@@ -21,7 +21,7 @@ const ContactSection = () => {
       formData.append("message", message);
 
       const res = await axios.post(
-        `http://localhost:8080/api/v1/portfolio-f/sendEmail`,
+        `https://portfolio-transformed.vercel.app/api/v1/portfolio-f/sendEmail`,
         formData,
         { headers: { "Content-Type": "application/json" } }
       );
